1339a257677aa95fff34f448bc64b2257077c7f7
Chris Warburton <chriswarbo@gmail.com>
2018-06-07 17:05:12 +0100

    Port getGroundTruths inner loop to Haskell
    The tests are passing so it seems to work. This doesn't improve our big-O time,
    and it doesn't avoid the startup overhead of Racket, but hopefully the constants
    will get better once we switch over completely to Haskell. After that we can
    start optimising the big-O, pre-computing things at compile time, etc.

--- nix-support/getGroundTruths.nix
+++ nix-support/getGroundTruths.nix
@@ -1,15 +1,102 @@
-{ fail, jq, makeSamples, runCommand, tebenchmark, withDeps, wrap }:
+{ fail, haskellPackages, jq, makeSamples, mkBin, runCommand, tebenchmark,
+  withDeps, wrap, writeScript }:
 
 with rec {
+  haskellVersion = runCommand "get-ground-truths-haskell"
+    {
+      buildInputs = [ (haskellPackages.ghcWithPackages (h: [
+        h.aeson h.atto-lisp h.attoparsec h.bytestring h.text
+      ])) ];
+      main = writeScript "get-ground-truths-main.hs" ''
+        {-# LANGUAGE OverloadedStrings #-}
+        module Main where
+
+        import           Control.Monad              (mzero)
+        import qualified Data.Aeson                 as A
+        import qualified Data.AttoLisp              as L
+        import qualified Data.Attoparsec.ByteString as AP
+        import qualified Data.ByteString.Char8      as B
+        import qualified Data.ByteString.Lazy       as LB
+        import qualified Data.Text                  as T
+        import qualified System.Environment         as Env
+        import           System.IO.Unsafe           (unsafePerformIO)
+
+        type TheoremID = String
+
+        newtype Name = N T.Text deriving (Eq, Show)
+
+        instance A.FromJSON Name where
+          parseJSON s = N <$> A.parseJSON s
+
+        -- Our cached data uses symbols, which may be escaped if they
+        -- contain e.g. "'". Escaped symbols are wrapped in pipes "|".
+        unescapeSym s = case T.stripPrefix "|" s of
+                          Nothing -> s
+                          Just s2 -> case T.stripSuffix "|" s2 of
+                            Nothing  -> s
+                            Just s3 -> s3
+
+        instance L.FromLisp Name where
+          parseLisp (L.Symbol n) = pure (N (unescapeSym n))
+          parseLisp _            = mzero
+
+        newtype TheoremDeps = TDs [(TheoremID, [Name])]
+
+        instance L.FromLisp TheoremDeps where
+          parseLisp l = do tds <- L.parseLisp l
+                           pure (TDs (map toPair tds))
+            where toPair [Left id, Right deps] = (id, deps)
+
+        theoremFilesAdmittedBy :: [Name] -> [TheoremID]
+        theoremFilesAdmittedBy sample = map fst (filter match theoremDeps)
+          where match td = snd td `subset` sample
+
+        theoremDeps :: [(TheoremID, [Name])]
+        theoremDeps = unsafePerformIO go
+          where go = do
+                  mp <- Env.lookupEnv name
+                  case mp of
+                    Nothing -> error ("Environment doesn't contain " ++ name)
+                    Just p  -> strToData <$> B.readFile p
+
+                name = "BENCHMARKS_THEOREM_DEPS"
+
+                lispToData l = case L.parseEither L.parseLisp l of
+                                 Left  e -> error e
+                                 Right d -> d
+
+                strToData  s = case AP.parseOnly (L.lisp <* AP.endOfInput) s of
+                                 Left  e -> error e
+                                 Right l -> lispToData l
+
+        subset []     ys = True
+        subset (x:xs) ys = (x `elem` ys) && (xs `subset` ys)
+
+        main = LB.interact (A.encode . theoremFilesAdmittedBy . parse)
+          where parse s = case A.eitherDecode s of
+                            Left  e -> error e
+                            Right x -> x
+      '';
+    }
+    ''
+      cp "$main" Main.hs
+      ghc --make -o Main Main.hs
+      mv Main "$out"
+    '';
+
   go = wrap {
     name   = "get-ground-truths.rkt";
-    paths  = [ tebenchmark.env ];
-    vars   = tebenchmark.cache;
+    paths  = [
+      tebenchmark.env
+      (mkBin { name = "haskellVersion"; file = haskellVersion; })
+    ];
+    vars   = tebenchmark.cache // { inherit haskellVersion; };
     script = ''
       #!/usr/bin/env racket
       #lang racket
       (require        json)
       (require        rackunit)
+      (require        shell/pipeline)
       (require        lib/conjectures)
       (require/expose lib/conjectures (theorem-files-admitted-by))
       (require        lib/normalise)
@@ -18,14 +105,19 @@ with rec {
       (define (err x)
         (error (format "~S" x)))
 
+      (define (theorems-of decoded-names)
+        (string->jsexpr
+          (run-pipeline/out `(echo ,(jsexpr->string decoded-names))
+                            '(haskellVersion))))
+
       ;; Ground truth for a sample (list of encoded names)
       (define (theorems-of-sample sample)
         (eprintf "theorems-of-sample\n")
         (unless (list? sample)
           (err `((error  "Expected sample to be a list")
                  (sample ,sample))))
-        (theorem-files-admitted-by (map (compose decode-name string->symbol)
-                                        sample)))
+        (theorems-of (map (compose symbol->string decode-name string->symbol)
+                          sample)))
 
       ;; Ground truth for a list of samples (e.g. buckets)
       (define (theorems-of-sample-list samples)
@@ -86,6 +178,7 @@ with rec {
       inherit go;
       buildInputs = [ fail jq ];
       samples     = makeSamples { sizes = [ 1 5 10 ]; reps = 3; };
+      smallSample = makeSamples { sizes = [ 2 ]; reps = 1; };
     }
     ''
       O=$(echo 'nope' | "$go" 2>&1) && fail "Non-JSON should error\n$O"
@@ -113,6 +206,12 @@ with rec {
         fail "Non-empty 'names' should match input '[\"global64\"]', got '$O'"
       echo "$O" | jq -e '.x | .theorems | . == []' ||
         fail "Bogus names should get empty 'theorems', got '$O'"
+
+      O=$("$go" < "$smallSample") || fail "Didn't get small ground truths"
+      echo "$O" | jq -e 'map(map(.sample | .theorems
+                                         | length
+                                         | . > 0) | all) | all' ||
+      fail "Small samples should have at least one theorem '$O'"
 
       O=$("$go" < "$samples") || fail "Didn't get ground truths of samples"
       echo "$O" | jq -e 'map(map(.sample | .theorems
