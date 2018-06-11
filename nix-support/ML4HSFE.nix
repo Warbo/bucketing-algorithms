f35adf2a92d2b523fc9c2a9da04cefc6b1541e5b
Chris Warburton <chriswarbo@gmail.com>
2018-06-11 16:17:31 +0100

    Don't invoke nix-shell for ML4HSFE test

--- nix-support/ML4HSFE.nix
+++ nix-support/ML4HSFE.nix
@@ -1,24 +1,25 @@
 # ML4HSFE executable from its Haskell package, with its test script checked
-{ haskellPackages, runCommand, runWeka, withDeps, withNix }:
+{ haskellPackages, jq, runCommand, runWeka, withDeps }:
 
 with {
   test = runCommand "ML4HSFE-test-suite"
-    (withNix {
+    {
       ml4hsfe     = haskellPackages.ML4HSFE.src;
       buildInputs = [
         (haskellPackages.ghcWithPackages (h: [
           h.cabal-install h.ghc h.ML4HSFE h.quickspec h.tasty h.tasty-quickcheck
         ]))
+        jq
         runWeka
       ];
-    })
+    }
     ''
       set -e
       export HOME="$PWD"
       cp -r "$ml4hsfe" ./ml4hsfe
       chmod +w -R ./ml4hsfe
       cd ./ml4hsfe
-      ./test.sh
+      bash ./test.sh
       mkdir "$out"
     '';
 };
