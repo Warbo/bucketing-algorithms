3674c863c7e73f77ee67667100941162dfa2cdb2
Chris Warburton <chriswarbo@gmail.com>
2018-07-23 16:13:26 +0100

    Force pinned versions to be used

--- nix-support/haskellTE.nix
+++ nix-support/haskellTE.nix
@@ -5,8 +5,9 @@ with {
   src = latestGit {
     url    = "https://github.com/Warbo/haskell-te.git";
     stable = {
-      rev    = "8904b29";
-      sha256 = "059h0jh01ay1kw2l0wh2i3sqni8bc4k3ankrh7cf0v624knbqb09";
+      rev        = "8904b29";
+      sha256     = "059h0jh01ay1kw2l0wh2i3sqni8bc4k3ankrh7cf0v624knbqb09";
+      unsafeSkip = false;
     };
   };
 };
--- nix-support/tebenchmark.nix
+++ nix-support/tebenchmark.nix
@@ -3,7 +3,8 @@
 import (latestGit {
   url    = http://chriswarbo.net/git/theory-exploration-benchmarks.git;
   stable = {
-    rev    = "eb9f5b9";
-    sha256 = "1jfqjc6s8lgp6ndqrhv4abzcsda2wrf2rwwj740y1lgam609wwzy";
+    rev        = "eb9f5b9";
+    sha256     = "1jfqjc6s8lgp6ndqrhv4abzcsda2wrf2rwwj740y1lgam609wwzy";
+    unsafeSkip = false;
   };
 }) {}
