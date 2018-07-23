4e23fcc16c1606a372a5173b23cbb4d115f34d1e
Chris Warburton <chriswarbo@gmail.com>
2018-07-23 15:10:58 +0100

    Remove local override of nix-helpers

--- nix-support/nix-helpers.nix
+++ nix-support/nix-helpers.nix
@@ -2,16 +2,12 @@
 
 with builtins;
 with rec {
-  local  = /home/chris/Programming/Nix/nix-helpers;
-
-  remote = fetchFromGitHub {
+  src = fetchFromGitHub {
     owner  = "Warbo";
     repo   = "nix-helpers";
     rev    = "96a2fa3";
     sha256 = "0j5xxgjbyjsj9ayj3q7b95s7gzmmahwlj27nvbmdjyrxk3dn7gxz";
   };
-
-  src  = if pathExists local then local else remote;
 
   oldImport = lib.fix (self: import path {
     config = { packageOverrides = import "${src}/overlay.nix" self; };
