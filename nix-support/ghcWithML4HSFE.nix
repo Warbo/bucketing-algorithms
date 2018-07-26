3367addca459ccd3f42f95ab44cbc469cb4d748c
Chris Warburton <chriswarbo@gmail.com>
2018-07-26 17:01:20 +0100

    Found working dependencies

--- nix-support/ghcWithML4HSFE.nix
+++ nix-support/ghcWithML4HSFE.nix
@@ -12,10 +12,15 @@ with rec {
     };
   };
 
-  getUrl = { name, self, sha256, url }: run {
-    inherit name self;
-    url = unpack (fetchurl { inherit url sha256; });
-  };
+  getHackage = { name, self, sha256, version }:
+    with { nv = "${name}-${version}"; };
+    run {
+      inherit name self;
+      url = unpack (fetchurl {
+        inherit sha256;
+        url = "https://hackage.haskell.org/package/${nv}/${nv}.tar.gz";
+      });
+    };
 
   run = { name, self, url }:
     self.callPackage (runCabal2nix { inherit name url; }) {};
@@ -26,10 +31,15 @@ with rec {
   # all dependencies of all our Haskell packages to be mutually compatible with
   # each other. If these packages' dependencies break other stuff, we don't need
   # to care since we're not using these for anything else.
-  hsPkgs = nixpkgs1803.haskell.packages.ghc802.override (old: {
+  hsPkgs = nixpkgs1803.haskell.packages.ghc7103.override (old: {
     overrides = nixpkgs1803.lib.composeExtensions
       (old.overrides or (_: _: {}))
       (self: super: {
+        # Tests can fail due to missing Arbitrary instances
+        aeson = haskell.lib.dontCheck super.aeson;
+
+        # Dependency of ML4HSFE. Note that this needs GHC 7.10, due to changes
+        # in GHC's package DB implementation.
         HS2AST = getRepo {
           inherit self;
           name   = "HS2AST";
@@ -38,6 +48,7 @@ with rec {
           sha256 = "1x2f12s6caj0gaymaw62bmm62ydim78wm2pn18j18fa2l3p7vqyi";
         };
 
+        # This is the package we want, all of the rest are dependencies.
         ML4HSFE = getRepo {
           inherit self;
           name   = "ML4HSFE";
@@ -46,30 +57,35 @@ with rec {
           sha256 = "1kcnhbkgfp0akp0g0jxh11f1zn96jybgl7rniwabhxpr9hszj3kn";
         };
 
-        # Needed by twee-lib
-        /*primitive = getUrl {
+        # If this is 2.10+ then quickspec 0.9.6 hits an "ambiguous 'total'"
+        # error. Version 2.9 hits problems elsewhere, with semigroups, instances
+        # and quickcheck-io packages.
+        QuickCheck = getHackage {
           inherit self;
-          name   = "primitive";
-          url    = "https://hackage.haskell.org/package/primitive-0.6.2.0/primitive-0.6.2.0.tar.gz";
-          sha256 = "1q9a537av81c0lvcdzc8i5hqjx3209f5448d1smkyaz22c1dgs5q";
-        };*/
-
-        # quickspec 0.9.6 encounters ambiguous 'total' with 2.10+, whilst 2.9
-        # needs semigroups
-        QuickCheck = getUrl rec {
-          inherit self;
-          name   = "QuickCheck";
-          url    = "https://hackage.haskell.org/package/QuickCheck-2.9.2/QuickCheck-2.9.2.tar.gz";
-          sha256 = "119np67qvx8hyp9vkg4gr2wv3lj3j6ay2vl4hxspkg43ymb1cp0m";
+          name    = "QuickCheck";
+          version = "2.8.2";
+          sha256  = "1ai6k5v0bibaxq8xffcblc6rwmmk6gf8vjyd9p2h3y6vwbhlvilq";
         };
 
-        # 2.1.5 fails by assuming semigroup pkg
-        /*twee-lib = getUrl {
+        # We use QuickSpec version 1 (0.9.6) since version 2+ is very different
+        quickspec = getHackage {
           inherit self;
-          name   = "twee-lib";
-          url    = "https://hackage.haskell.org/package/twee-lib-2.1.4/twee-lib-2.1.4.tar.gz";
-          sha256 = "0fapi6g8f6fp90hvff1g606wzi7jaacf1mnq1h801yzzi4sj3yms";
-        };*/
+          name    = "quickspec";
+          version = "0.9.6";
+          sha256  = "0prwzxsrvfqryl75rmma229d4y7ra61vc3d72kyqi4l44ga2ay21";
+        };
+
+        # This package's dependencies differ depending on whether we're using
+        # GHC 7 (semigroups dependency is added) or GHC 8 (no semigroups
+        # dependency). The nixpkgs package is the same across all of its Haskell
+        # package sets; in particular, no semigroups dependency is passed in,
+        # even if we're using GHC 7 that needs it.
+        system-filepath = getHackage {
+          inherit self;
+          name    = "system-filepath";
+          version = "0.4.13.4";
+          sha256  = "1yy5zsmmimhg6iaw9fmpwrxvxrgi5s6bfyqfihdsnx4bjvn7sp9l";
+        };
       });
   });
 
