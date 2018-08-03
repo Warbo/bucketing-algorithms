7205ebe189354470ae4697bed980508daf06c396
Chris Warburton <chriswarbo@gmail.com>
2018-08-03 18:17:31 +0100

    Check python syntax

--- check.sh
+++ check.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+function filesSuffixed {
+    find . -name "*$1" | grep -v '/\.asv/env/'
+}
+
 # Simple, quick sanity check. Useful as a git pre-commit hook.
 
-find . -name "*.nix" | grep -v '/\.asv/env/' | while read -r F
+filesSuffixed ".nix" | while read -r F
 do
     echo "Checking syntax of '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null
+done
+
+filesSuffixed ".py" | while read -r F
+do
+    echo "Checking syntax of '$F'" 1>&2
+    python -m py_compile "$F"
 done
