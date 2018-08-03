efccec64edcaf8ece2634cbd8b276969e1be40a5
Chris Warburton <chriswarbo@gmail.com>
2018-08-03 14:10:47 +0100

    Take asv and asv-nix from warbo-packages
    We still override their pythonPackages to use Python 3, but now we get important
    updates like the '--yes' option to 'asv machine', which simplifies automated
    benchmark running.

--- benchmarks/asv-nix.nix
+++ benchmarks/asv-nix.nix
@@ -1,16 +1,6 @@
-{ asv, callPackage, fetchFromGitHub, python3Packages }:
+{ asv, python3Packages, warbo-packages }:
 
-with {
+warbo-packages.asv-nix.override {
+  asv            = asv.override { pythonPackages = python3Packages; };
   pythonPackages = python3Packages;
-
-  src = fetchFromGitHub {
-    owner  = "Warbo";
-    repo   = "asv-nix";
-    rev    = "d5af74d";
-    sha256 = "1jp5a8p5dzh2vb2s9k2wf3j2l9fcm7l47ydqy8wlrjiyqlc4jw7a";
-  };
-};
-callPackage "${src}" {
-  inherit pythonPackages;
-  asv = asv.override { inherit pythonPackages; };
 }
