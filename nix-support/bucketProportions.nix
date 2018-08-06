7c4e451034e3d03b8c87d07528aab794a0fd720f
Chris Warburton <chriswarbo@gmail.com>
2018-08-06 19:07:35 +0100

    Factor out script from its runner

--- nix-support/bucketProportions.nix
+++ nix-support/bucketProportions.nix
@@ -15,48 +15,50 @@ given: with rec {
 
   # Runs each sample through the stdio of a given program, adding the result to
   # the samples JSON. Useful for running a bucketing script on each sample.
+  processSamplesScript = { key, prog }: wrap {
+    name   = "process-samples.py";
+    paths  = [ python3 ];
+    vars   = { inherit key prog; LANG = "en_US.UTF-8"; };
+    script = ''
+      #!/usr/bin/env python3
+      from io         import StringIO
+      from json       import dumps, loads
+      from os         import getenv
+      from subprocess import check_output
+      from sys        import stderr, stdin
+
+      msg  = lambda x: stderr.write(repr(x) + '\n')
+
+      sort = lambda collection: sorted([elem for elem in collection])
+
+      key, prog = map(getenv, ['key', 'prog'])
+
+      process = lambda names: loads(check_output(
+        [prog],
+        input=dumps(names).encode('utf-8')).decode('utf-8'))
+
+      def recurse(path, val):
+        if val is None:
+          return None
+        if type(val) == type({}):
+          if 'sample' in val:
+            return dict(val, **{key: recurse(path + ['sample'],
+                                             val['sample'])})
+          return {k: recurse(path + [k], val[k]) for k in sort(val)}
+        if path != [] and path[-1] == 'sample':
+          return process(val)
+        return val
+
+      data = loads(stdin.read())
+
+      print(dumps(recurse([], data)))
+    '';
+  };
+
   processSamples = { key, prog, samples }: runCommand "processed-${key}.json"
     {
       inherit samples;
-      script = wrap {
-        name   = "process-samples.py";
-        paths  = [ python3 ];
-        vars   = { inherit key prog; LANG = "en_US.UTF-8"; };
-        script = ''
-          #!/usr/bin/env python3
-          from io         import StringIO
-          from json       import dumps, loads
-          from os         import getenv
-          from subprocess import check_output
-          from sys        import stderr, stdin
-
-          msg  = lambda x: stderr.write(repr(x) + '\n')
-
-          sort = lambda collection: sorted([elem for elem in collection])
-
-          key, prog = map(getenv, ['key', 'prog'])
-
-          process = lambda names: loads(check_output(
-            [prog],
-            input=dumps(names).encode('utf-8')).decode('utf-8'))
-
-          def recurse(path, val):
-            if val is None:
-              return None
-            if type(val) == type({}):
-              if 'sample' in val:
-                return dict(val, **{key: recurse(path + ['sample'],
-                                                 val['sample'])})
-              return {k: recurse(path + [k], val[k]) for k in sort(val)}
-            if path != [] and path[-1] == 'sample':
-              return process(val)
-            return val
-
-          data = loads(stdin.read())
-
-          print(dumps(recurse([], data)))
-        '';
-      };
+      script = processSamplesScript { inherit key prog; };
     }
     ''
       "$script" < "$samples" > "$out"
