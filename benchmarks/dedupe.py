88681e266ea66b6c77376c5c56c3f457b136bef6
Chris Warburton <chriswarbo@gmail.com>
2018-08-07 18:44:46 +0100

    We can't dedupe individual reps; only the whole lot

--- benchmarks/dedupe.py
+++ benchmarks/dedupe.py
@@ -1,14 +1,13 @@
-from timeit     import default_timer
-from .util      import load_command, load_samples, run_on
+from os     import getenv
+from timeit import default_timer
+from .util  import load_command, load_samples, run_on
 
-cmd     = load_command('dedupeSamples')
-samples = load_samples()
+cmd = load_command('dedupeSamples')
 
-def time_dedupe(size):
-    key = str(size)
-    for rep in samples[key]:
-        run_on([cmd], samples[key][rep])
+with open(getenv('samples'), 'r') as f:
+  samples = f.read()
 
-time_dedupe.param_names = ['size']
-time_dedupe.params      = [sorted([int(s) for s in samples.keys()])]
-time_dedupe.timer       = default_timer  # Wall-clock rather than CPU time
+def time_dedupe():
+    run_on([cmd], samples)
+
+time_dedupe.timer = default_timer  # Wall-clock rather than CPU time
