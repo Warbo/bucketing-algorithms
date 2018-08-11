95aaa71634b4005684cc7bfa83de46d5b24b3cc6
Chris Warburton <chriswarbo@gmail.com>
2018-08-11 21:04:08 +0100

    Add bucketSizes to Bucketutil

--- haskell-support/BucketUtil.hs
+++ haskell-support/BucketUtil.hs
@@ -44,3 +44,41 @@ clusters asts = fromJust (fromSize <|> fromEnv <|> Just fromIn)
         inCount = length asts
         ceil    = ceiling :: Float -> Int
 
+newtype Method = Method String
+
+type Bucketer = (Method, Maybe Int -> [AST] -> [[AST]])
+
+type Bucketed = HM.HashMap Method (HM.HashMap Int [[Name]])
+
+bucketSizes :: [Int] -> [AST] -> Bucketer -> Bucketed
+bucketSizes sizes asts (method, bucket) = HM.singleton method results
+  where results = fromList (map go sizes)
+        go size = (size, map (map getName) (bucket (Just size) asts))
+
+-- For aggregated samples
+
+newtype Sizes = Sizes (HM.HashMap Int -> Size)
+
+newtype Size  = Size  (HM.HashMap Int -> Maybe Rep)
+
+newtype Rep   = Rep { sample :: [Name], bucketed :: Bucketed }
+
+instance A.ToJSON Rep where
+  toJSON (Rep sample bucketed) =
+    let meth (Method m, bucketed) = m .= object (map size (HM.toList bucketed))
+        size (i, names)           = T.pack (show i) .= map (map unName) names
+     in A.object (("sample" .= sample):map meth (HM.toList bucketed))
+
+instance A.ToJSON Size where
+  toJSON (Size m) = A.toJSON m
+
+instance A.ToJSON Sizes where
+  toJSON (Sizes m) = A.toJSON m
+
+bucketAll :: [Bucketer] -> ([Name] -> [AST]) -> Sizes -> Sizes
+bucketAll brs astsOf (Sizes ss) = Sizes (HM.map goSize ss)
+  where goSize (Size s) = Size (HM.map goRep s)
+        goRep r = case r of
+          Nothing         -> Nothing
+          Just (Rep s bs) -> Just (Rep s (HM.unions (bs:map (bucket s) brs)))
+        bucket sample = bucketSizes [1..20] (astsOf sample)
