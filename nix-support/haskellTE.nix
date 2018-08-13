7081d7a798edc5ebd59843f5877cb09115a09ade
Chris Warburton <chriswarbo@gmail.com>
2018-08-13 01:10:17 +0100

    Propagate our 'nix' package through to haskell-te

--- nix-support/haskellTE.nix
+++ nix-support/haskellTE.nix
@@ -1,11 +1,19 @@
 # Useful for ASTs, etc.
-{ latestGit }:
+{ latestGit, nix, nix-helpers }:
 
-import (latestGit {
-  url    = "https://github.com/Warbo/haskell-te.git";
-  stable = {
-    rev        = "c68e6fd";
-    sha256     = "14ji8kzk6z477kq8vf633iqm57v1z9byz630mmq51ppikvgjmisi";
-    unsafeSkip = false;
+with {
+  src = latestGit {
+    url    = "https://github.com/Warbo/haskell-te.git";
+    stable = {
+      rev        = "c68e6fd";
+      sha256     = "14ji8kzk6z477kq8vf633iqm57v1z9byz630mmq51ppikvgjmisi";
+      unsafeSkip = false;
+    };
   };
-}) {}
+};
+import nix-helpers.repo1803 {
+  overlays = [
+    (import "${src}/overlay.nix")
+    (self: super: { inherit nix; })
+  ];
+}
