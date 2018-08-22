bb1a2982aa1476d23ed78e7ec293e9a7a0319708
Chris Warburton <chriswarbo@gmail.com>
2018-08-22 14:01:59 +0100

    Use lzip on bucketed JSON, since it's huge (~1.5GB)

--- nix-support/proportionExperiment.nix
+++ nix-support/proportionExperiment.nix
@@ -1,32 +1,44 @@
 # Performs bucketing on a large set of samples, gets their ground truths,
 # calculates the proportion remaining after bucketing and averages them.
-{ attrsToDirs', bucketProportions, makeSamples, runOn }:
+{ attrsToDirs', bash, bucketProportions, composeBins, lzip, makeSamples, runOn,
+  wrap }:
 
 with bucketProportions;
 with rec {
+  lz = "${lzip}/bin/lzip";
+
+  unlz = wrap {
+    name   = "unlzip";
+    paths  = [ bash lzip ];
+    script = ''
+      #!/usr/bin/env bash
+      exec lzip -d "$@"
+    '';
+  };
+
   go = { label ? "results", maxSize ? 100, reps ? 100 }: rec {
     steps = {
       "samples.json" = makeSamples { inherit maxSize reps; };
 
-      "withBuckets.json" =
+      "withBuckets.json.lz" =
         runOn "with-buckets"
-              addBuckets
+              (composeBins "add-buckets-lzip" [ addBuckets lz ])
               steps."samples.json";
 
-      "withBucketsGroundTruths.json" =
+      "withBucketsGroundTruths.json.lz" =
         runOn "with-ground-truths"
-              getGroundTruths
-              steps."withBuckets.json";
+              (composeBins "add-ground-truths-lzip" [ unlz getGroundTruths lz ])
+              steps."withBuckets.json.lz";
 
-      "withBucketsGroundTruthsProportions.json" =
+      "withBucketsGroundTruthsProportions.json.lz" =
         runOn "with-proportions"
-              calculateProportions
-              steps."withBucketsGroundTruths.json";
+              (composeBins "proportions-lzip" [ unlz calculateProportions lz ])
+              steps."withBucketsGroundTruths.json.lz";
 
-      "averageBucketProportions.json" =
+      "averageBucketProportions.json.lz" =
         runOn "with-averages"
-              averageProportions
-              steps."withBucketsGroundTruthsProportions.json";
+              (composeBins "average-lzip" [ unlz averageProportions lz ])
+              steps."withBucketsGroundTruthsProportions.json.lz";
     };
 
     results = attrsToDirs' "proportion-experiment-${label}" steps;
