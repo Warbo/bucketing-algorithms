5a5391bc0694e4a8f2db19f3b2fafc2df82f81f9
Chris Warburton <chriswarbo@gmail.com>
2018-09-10 07:22:13 +0100

    Use messagepack

--- nix-support/proportionExperiment.nix
+++ nix-support/proportionExperiment.nix
@@ -1,6 +1,7 @@
 # Performs bucketing on a large set of samples, gets their ground truths,
 # calculates the proportion remaining after bucketing and averages them.
-{ attrsToDirs', bash, bucketProportions, composeBins, lzip, makeSamples, runOn,
+{ attrsToDirs', bash, bucketProportions, callPackage, composeBins, coreutils,
+  lzip, makeSamples, msgpack-tools, nixpkgs1803, pv, runCommand, runOn,
   withDeps, wrap }:
 
 with bucketProportions;
@@ -30,15 +31,35 @@ with rec {
               (composeBins "add-ground-truths-lzip" [ unlz getGroundTruths lz ])
               steps."withBuckets.json.lz";
 
-      "withBucketsGroundTruthsProportions.json.lz" =
+      "withBucketsGroundTruths.msgpack.lz" = runCommand
+        "withBucketsGroundTruths.msgpack.lz"
+        {
+          buildInputs = [
+            msgpack-tools
+            coreutils
+            lzip
+            pv  # Give progress bars
+          ];
+          f = steps."withBucketsGroundTruths.json.lz";
+        }
+        ''
+          echo "Extracting '$f' temporarily" 1>&2
+          pv -f -p -e "$f" | lzip -d > extracted.json
+
+          echo "Converting JSON to MsgPack" 1>&2
+          S=$(stat --printf="%s" "$f")
+          json2msgpack extracted.json | lzip | pv -f -p -e -s "$S" > "$out"
+        '';
+
+      "withBucketsGroundTruthsProportions.msgpack.lz" =
         runOn "with-proportions"
               (composeBins "proportions-lzip" [ unlz calculateProportions lz ])
-              steps."withBucketsGroundTruths.json.lz";
+              steps."withBucketsGroundTruths.msgpack.lz";
 
-      "averageBucketProportions.json.lz" =
+      "averageBucketProportions.msgpack.lz" =
         runOn "with-averages"
               (composeBins "average-lzip" [ unlz averageProportions lz ])
-              steps."withBucketsGroundTruthsProportions.json.lz";
+              steps."withBucketsGroundTruthsProportions.msgpack.lz";
     };
 
     results = attrsToDirs' "proportion-experiment-${label}" steps;
