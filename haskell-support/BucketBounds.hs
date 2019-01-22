7005da443c6148f4ec3cef6e92785e89633e5b6a
Chris Warburton <chriswarbo@gmail.com>
2019-01-22 19:04:51 +0000

    Make BucketBounds.hs run MiniZinc. Output isn't parsed properly yet!

--- haskell-support/BucketBounds.hs
+++ haskell-support/BucketBounds.hs
@@ -1,118 +1,278 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings, PartialTypeSignatures #-}
 module BucketBounds where
 
-import qualified Control.Lens      as Lens
+import qualified Control.Lens               as Lens
 import           Control.Lens.Operators
-import           Control.Lens.Type (IndexedTraversal', Lens', Prism', Traversal')
-import           Data.Aeson        (toJSON)
+import           Control.Lens.Type          (Lens', Prism', Traversal')
+import           Data.Aeson                 (toJSON)
 import           Data.Aeson.Lens
-
-{-
-import qualified BucketUtil                 as BU
-import           Control.Monad.State.Strict (get, put, replicateM_, runState, State)
--}
 import qualified Data.Aeson                 as A
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Data.Char                  as C
-import           Data.List                  ((\\), intercalate, nub, sort)
+import qualified Data.HashMap               as HM
+import           Data.List                  ((\\), intercalate, isInfixOf, nub, sort)
 import qualified Data.Map.Strict            as Map
 import qualified Data.Maybe                 as M
+import           Data.Ratio                 ((%))
 import qualified Data.Text                  as T
 import qualified Data.Vector                as V
 import qualified Data.Vector.Lens           as V
-{-
-import qualified Data.IORef                 as IR
-import           Data.Maybe                 (fromMaybe)
+import qualified GetGroundTruths            as GGT
+import qualified Helper                     as GGTH
 import           System.Environment         (lookupEnv)
-import           System.IO                  (openFile, IOMode(ReadMode))
--}
-import           Test.QuickCheck            hiding (sample)
+import           System.Exit                (ExitCode(ExitSuccess, ExitFailure))
+import           System.IO                  (hPutStrLn, openFile, IOMode(ReadMode), stderr)
+import qualified System.Process             as P
+import           Test.QuickCheck            hiding (sample, Result)
 import           Test.Tasty                 (defaultMain, localOption, testGroup)
 import           Test.Tasty.QuickCheck      (testProperty)
 
+-- Inputs
 type Size      = Int
 type Name      = T.Text
 type Sample    = V.Vector Name
 type RawSample = V.Vector A.Value
 
-{-
-boundsMain :: IO ()
-boundsMain = do
-  jsonFileM <- lookupEnv "SAMPLE_JSON"
-  let jsonFile = fromMaybe (error "No SAMPLE_JSON env var given, aborting")
-                           jsonFileM
-  file    <- openFile jsonFile ReadMode
-  content <- BS.hGetContents file
-  imp     <- bsImp content
-  process imp
+-- Intermediate
+type BucketSize = Int
 
--- Turns {Size: {Rep: Sample}} into {Size: {Rep: {Count: Proportion}}}
-process :: Monad m => BU.StreamImp m -> m ()
-process imp = BU.streamKeyVals' imp (processSize imp)
+-- Outputs
+type Count       = Int
+type Proportion  = Float
+type Proportions = Map.Map Count Proportion
 
--- Turns (Size, {Rep: Sample}) into {Rep: {Count: Proportion}}
-processSize :: Monad m => BU.StreamImp m -> BS.ByteString -> m ()
-processSize imp sizeStr = BU.streamKeyVals' imp (processRep imp size)
-  where size :: Int
-        size = read (BS.unpack sizeStr)
+bucketSizeForBucketCount :: Size -> Count -> BucketSize
+bucketSizeForBucketCount size count = ceiling (size % count)
 
--- Turns (Size, Sample) into {Count: Proportion}
-processRep :: Monad m => BU.StreamImp m -> Int -> BS.ByteString -> m ()
-processRep imp size repStr = BU.streamKeyVals' imp (processSample size)
+prop_bucketSize = testProperty "Can calculate bucket sizes to split up samples"
+                               go
+  where go (Positive size) (Positive count) = size >= count ==> go' size count
 
-processSample = undefined
+        go' size count = let bucketSize  = bucketSizeForBucketCount size count
+                             capacity    = count *  bucketSize
+                             lowerCap    = count * (bucketSize - 1)
+                             bigEnough   = capacity >= size
+                             smallEnough = lowerCap <  size
+                             result      = property (bigEnough && smallEnough)
+                             info        = (("size"       , size       ),
+                                            ("count"      , count      ),
+                                            ("capacity"   , capacity   ),
+                                            ("lowerCap"   , lowerCap   ),
+                                            ("bigEnough"  , bigEnough  ),
+                                            ("smallEnough", smallEnough))
+                          in counterexample (show info) result
 
-bsImp :: BS.ByteString -> IO (BU.StreamImp IO)
-bsImp = fmap mkImp . IR.newIORef
-  where mkImp ref = BU.StreamImp {
-          BU.getchar     = do c <- fmap BS.head (IR.readIORef ref)
-                              IR.modifyIORef' ref BS.tail
-                              pure c,
+-- Like 'map f xs' but also gives an index, starting from 1
+imap1 :: (Enum a, Num a) => (a -> b -> c) -> [b] -> [c]
+imap1 f = zipWith f [1..]
 
-          BU.getcontents = IR.readIORef ref,
-          BU.info        = BU.debug,
-          BU.putchar     = putChar,
-          BU.putstr      = BS.putStr
-        }
+mapLines f = unlines . map f
 
-prop_processHandlesJSON = testProperty "JSON is handled by process"
-                                       (forAll genSizes goSizes)
+data Bound = Min | Max
 
-  where goSizes ss    = forAll (genFromSizes ss) (go ss)
-        go sizes json = undefined (BU.runOn :: _) False (process BU.testImp)
+instance Show Bound where
+  show Min = "Min"
+  show Max = "Max"
 
-genSizes :: Gen [Int]
-genSizes = listOf (choose 1 20)
+renderModel :: Bound -> Int -> Int -> Sample -> [[Name]] -> String
+renderModel bound bCount bSize sample theorems = unlines [
+    "% " ++ show (("bound"      , bound          ),
+                  ("bCount"     , bCount         ),
+                  ("bSize"      , bSize          ),
+                  ("sample"     , sample         ),
+                  ("theorems"   , theorems       ),
+                  ("theoremDeps", map (\(x, y) -> (x, noAsc y)) GGT.theoremDeps)),
 
-genFromSizes :: [Int] -> Gen BS.ByteString
-genFromSizes = pure . A.toJSON . map go
-  where go :: [Int] -> [(Int, ())]
-        go s = (s, ())
--}
+    "include \"globals.mzn\";",
 
---_sample :: Traversal' t Sample
-{-_sample = members      . _Object .  -- Each size entry
-          members      . _Object .  -- Each rep entry
-          key "sample" . _Array  .  -- The sample (array of names)
-          from V.vector          .  -- Convert to a list
-          each           _String    -- Those names which are strings
--}
+    "% Declare bCount buckets, as sets of elements from 1 to nameCount",
+    let declareBucket n = "var " ++ typeS ++ ": " ++ bucket n ++ ";"
+     in mapLines declareBucket [1..bCount],
 
---_sizes :: _
---_sizes = members . _Object
+    "% Constrain bucket sizes (cardinalities)",
+    "% bucket1 contains the remainder (which may be zero)",
+    "constraint card(bucket1) = " ++ bucketRemainder ++ ";",
 
---"{\"a\": 4, \"b\": 7}" & members . _Number *~ 10
+    "% All other buckets are constrained to be of size bSize",
+    let bSizeS      = show bSize
+        constrain n = "constraint card(" ++ bucket n ++ ") = " ++ bSizeS ++ ";"
+     in mapLines constrain [2..bCount],
 
--- s = Raw, a = Sample
--- (a -> s) -> (s -> Either s a) -> Prism' s a
+    "% Stop buckets overlapping (one constraint per distinct x/y intersection)",
+    let disjoint x y | x >= y = ""  -- Avoid x == y and redundant constraints
+        disjoint x y          = let bX        = "bucket" ++ show x
+                                    bY        = "bucket" ++ show y
+                                    intersect = bX ++ " intersect " ++ bY
+                                 in "constraint card(" ++ intersect ++ ") = 0;"
+     in mapLines (\x -> mapLines (disjoint x) [1..bCount])
+                 [1..bCount],
+
+    "% Define each theorem as a function, taking a bucket and returning 1 if",
+    "% its dependencies are all in that bucket and 0 otherwise",
+    let mkFunc theorem = let arg    = "(var " ++ typeS ++ ": bucket)"
+                             sig    = "function var int: " ++ fst theorem ++ arg
+                             isIn d = "(" ++ show d ++ " in bucket)"
+                             cond   = sepStrings " /\\ " isIn (snd theorem)
+                             body   = "if (" ++ cond ++ ") then 1 else 0 endif"
+                          in sig ++ " = " ++ body ++ ";"
+     in mapLines mkFunc deps,
+
+    "% Define the score of a bucket as the sum of all theorem functions",
+    let sig          = "function var int: score(var " ++ typeS ++ ": bucket)"
+        call theorem = "(" ++ fst theorem ++ "(bucket))"
+        body         = sepStrings " + " call deps
+     in sig ++ " = " ++ body ++ ";",
+
+    "% Tell MiniZinc to min/maximise the sum of all bucket scores",
+    let direction = case bound of
+                      Max -> "maximize"
+                      Min -> "minimize"
+        scoreOf n = "score( " ++ bucket n ++ " )"
+        sum       = sepStrings " + " scoreOf [1..bCount]
+     in "solve " ++ direction ++ " " ++ sum ++ ";",
+
+    "% Turn our integer names back into strings by looking up in an array",
+    "array[1..", nameCountS, "] of string: names;",
+    let quoted n = "\"" ++ n ++ "\""
+     in "names = [ " ++ sepStrings ", " quoted names ++ " ];",
+
+    "% Render chosen buckets to output",
+    let showBucket n = "show([ names[i] | i in " ++ bucket n ++ " ])"
+     in "output [" ++ sepStrings ", \"\\n\", " showBucket [1..bCount] ++ "];"]
+  where nameCount       = V.length sample
+        bucketRemainder = show (nameCount `mod` bSize)
+
+        nameCountS      = show   nameCount
+
+        typeS    = "set of 1.." ++ nameCountS
+        bucket n = "bucket" ++ show n
+
+        names = map T.unpack (V.toList sample)
+
+        -- Since we models the names as numbers 1..nameCount, we convert the
+        -- deps of each theorem into their corresponding numbers too.
+        deps = let indices :: Map.Map T.Text Int
+                   indices      = Map.fromList (imap1 (flip (,))
+                                                      (V.toList sample))
+                   collect i ds = ("theorem" ++ show i,
+                                   map (\n -> M.fromJust (Map.lookup n indices))
+                                       ds)
+                in imap1 collect theorems
+
+runModel :: String -> IO String
+runModel model = P.readProcessWithExitCode "mzn-fzn"
+                                           ["-f", "fzn-gecode", "-"]
+                                           model >>= go
+  where go (code, out, err) = case code of
+                                ExitSuccess   -> (if "UNSATISFIABLE" `isInfixOf` out
+                                                     then putStrLn ("\n\nMODEL\n\n" ++ model ++ "\n\nEND MODEL\n\n")
+                                                     else pure ()) >> pure out
+                                ExitFailure x -> putStrLn (concat [
+                                                   "EXIT CODE\n\n",
+                                                   show x,
+                                                   "\n\nSTDOUT\n\n",
+                                                   out,
+                                                   "\n\nSTDERR\n\n",
+                                                   err,
+                                                   "\n\nMODEL\n\n",
+                                                   model]) >> error "DIE"
+
+bucketCounts = [1..20]
+
+data Result = Result {
+    minimise :: Proportion,
+    maximise :: Proportion
+  }
+
+runForAllCounts :: Sample -> IO (Map.Map Count (Either String Result))
+runForAllCounts sample = Map.fromList <$> mapM go possibleCounts
+  where go bucketCount = let size   = bucketSize bucketCount
+                             run  b = runModel (renderModel b
+                                                            bucketCount
+                                                            size
+                                                            unhexed
+                                                            theorems)
+                          in do min <- run Min
+                                max <- run Max
+                                let min' = parseResult min
+                                    max' = parseResult max
+                                showErr min'
+                                showErr max'
+                                pure (bucketCount, Result <$> min' <*> max')
+
+        possibleCounts = filter (<= sampleSize) bucketCounts
+
+        showErr (Left err) = writeErr err
+        showErr _          = pure ()
+
+        unhexed = V.map (unN . GGTH.decodeName . GGTH.N) sample
+
+        sampleSize = length sample
+
+        bucketSize = bucketSizeForBucketCount sampleSize
+
+        theorems :: [[Name]]
+        theorems = theoremsOf unhexed
+
+writeErr :: String -> IO ()
+writeErr = hPutStrLn stderr
+
+parseResult :: String -> Either String Proportion
+parseResult = Left  -- FIXME
+
+theoremsOf :: Sample -> [[Name]]
+theoremsOf sample = check                            .
+                    --map unasc                        .
+                    filter (`GGTH.subset` ascSample) $
+                    unwrappedTheoremDeps
+  where ascSample = GGTH.mkAscendingList (V.toList sample)
+        check l = if null l
+                     then error (show (
+                            ("error", "Sample should admit at least 1 theorem"),
+                            ("sample", sample),
+                            ("deps",   unwrappedTheoremDeps)))
+                     else l
+
+unwrappedTheoremDeps = map depsOf GGT.theoremDeps
+  where depsOf = map unN . unasc . snd
+
+unN (GGTH.N x) = x
+
+unasc (GGTH.AscendingList x) = x
+
+noAsc = map unN . unasc
+
+sepStrings :: String -> (a -> String) -> [a] -> String
+sepStrings sep f = intercalate sep . map f
+
+processSample :: Sample -> IO A.Value
+processSample s = do r <- rendered
+                     pure (A.object ["sample"      A..= s,
+                                     "proportions" A..= r])
+  where rendered :: IO A.Value
+        rendered = A.toJSON . Map.map render <$> runForAllCounts s
+
+        render (Left  s) = A.object ["ok" A..= False, "error" A..= s         ]
+        render (Right r) = A.object ["ok" A..= True , "min"   A..= minimise r,
+                                                      "max"   A..= maximise r]
+
 decodeSample :: Prism' RawSample Sample
-decodeSample = Lens.prism encode decode
-  where decode raw = let l  = V.toList raw
-                         l' = M.catMaybes (map (^? _String) l)
-                      in if length l == length l'
-                            then Right (V.fromList l')
-                            else Left  raw
-        encode     = V.map A.String
+decodeSample = Lens.prism encodeSample decodeSample''
+
+decodeSample' :: A.Value -> Either A.Value Sample
+decodeSample' (A.Array v) = case decodeSample'' v of
+                              Left  x -> Left  (A.Array x)
+                              Right x -> Right x
+
+decodeSample'' :: RawSample -> Either RawSample Sample
+decodeSample'' = go  {-. _Array-}
+  where go raw = let l  = V.toList raw
+                     l' = M.catMaybes (map (^? _String) l)
+                  in if length l == length l'
+                        then Right (V.fromList l')
+                        else Left  raw
+
+encodeSample = V.map A.String
 
 sample :: AsValue a => Traversal' a Sample
 sample = key "sample" . _Array . decodeSample
@@ -123,17 +283,31 @@ repSamples = members . sample
 sizeSamples :: AsValue a => Traversal' a Sample
 sizeSamples = members . repSamples
 
-{-
-_sizes :: Traversal' _ A.Value
-_sizes = traverse
+sizeSamples' :: AsValue a => Traversal' a A.Value
+sizeSamples' = members . members . key "sample"
 
-_sizeSamples = _sizes . repSamples
--}
+processSample' :: A.Value -> IO A.Value
+processSample' (A.Array v) = case decodeSample'' v of
+                               Left  x -> error ("Couldn't decode " ++ show x)
+                               Right s -> processSample s
+processSample' x = error ("processSample' needed an Array, given " ++ show x)
 
-boundsMain = error "NOT IMPLEMENTED"
+boundsMain :: IO ()
+boundsMain = do
+  jsonFileM <- lookupEnv "SAMPLE_JSON"
+  let jsonFile = M.fromMaybe (error "No SAMPLE_JSON env var given, aborting")
+                             jsonFileM
+  file <- openFile jsonFile ReadMode
+  -- WE NEED TO READ IN THE JSON, AND USE A LENS OPERATION LIKE over TO MODIFY
+  -- EACH OF THE SAMPLES, REPLACING THEM WITH A PROCESSED VERSION. WE NEED IO'S
+  -- APPLICATIVE INSTANCE TO DO THAT, AND A FUNCTION TO ACTUALLY PIPE THE DATA
+  -- THROUGH MiniZinc
+  BS.hGetContents file >>= Lens.traverseOf sizeSamples' processSample'
+                       >>= BS.putStr
 
 boundsTest = defaultMain $ testGroup "All tests" [
     prop_collate,
+    prop_bucketSize,
     prop_getSample,
     prop_getReps,
     prop_getSizes
