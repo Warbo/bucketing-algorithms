43204ebe35e35f74e8a78e98191170fb89b15206
Chris Warburton <chriswarbo@gmail.com>
2019-02-01 11:10:53 +0000

    Build bucketBound data (but only up to size 10)
    It turns out that MiniZinc takes *way* too long on sizes 11+, so I've limited it
    to 10 for now.

--- nix-support/bucketBounds.nix
+++ nix-support/bucketBounds.nix
@@ -1,5 +1,5 @@
-{ attrsToDirs', ghcWithML4HSFE, lib, nixpkgs1703, nixpkgs1803, runCommand,
-  tebenchmark, wrap, writeScript }:
+{ attrsToDirs', extractSampleJSON, ghcWithML4HSFE, jq, lib, nixpkgs1703,
+  nixpkgs1803, runCommand, tebenchmark, wrap, writeScript }:
 
 with builtins;
 with lib;
@@ -41,5 +41,35 @@ with rec {
       main = "boundsMain";
     };
   };
+
+  data = runCommand "bucketBounds"
+    {
+      inherit bucketBoundRunner;
+      buildInputs = [ jq ];
+      extracted   = extractSampleJSON;
+    }
+    ''
+      # Start off with an empty object (no sizes)
+      echo '{}' > result.json
+
+      export SAMPLE_JSON="$PWD/input.json"
+      for SIZE in $(seq 1 10)
+      do
+        echo "Extracting samples of size $SIZE" 1>&2
+        jq --arg size "$SIZE" '{($size) : .[$size]}' < "$extracted" \
+                                                     > "$SAMPLE_JSON"
+
+        echo "Processing size $SIZE" 1>&2
+        date 1>&2
+        "$bucketBoundRunner" > output.json
+        date 1>&2
+
+        # Merge this size into the result
+        jq --argfile result result.json '$result * .' < output.json > temp.json
+        mv temp.json result.json
+      done
+
+      jq 'map_values(map_values(.sample))' < result.json > "$out"
+    '';
 };
-bucketBoundRunner
+data
