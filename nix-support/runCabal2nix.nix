c79a424e8f34b977d73324b1dfe87057b31e710b
Chris Warburton <chriswarbo@gmail.com>
2019-11-09 21:34:06 +0000

    Remove unused runCabal2nix

--- nix-support/runCabal2nix.nix
+++
@@ -1,6 +0,0 @@
-# Generates derivations which run cabal2nix, which we can import to get a usable
-# Haskell package function. We take this from nix-helpers, but override the
-# cabal2nix version to that of nixpkgs1803 since that accepts a --compiler arg.
-{ cabal2nix, nix-helpers }:
-
-nix-helpers.runCabal2nix.override { inherit cabal2nix; }
